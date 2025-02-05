0c41c917d6b2c83a8931d834568f14b08c96900d
M3hTi <mehdinasir502@yahoo.com>
2025-02-05 14:02:12 +0330

    feat: add dark mode toggle functionality to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import * as React from 'react'
-import axios from 'axios'
 
 import './App.css'
 
 function App() {
+  const [isDark, setIsDark] = React.useState(false)
+
+  const toggle = React.useCallback(() => {
+    setIsDark(prevState => !prevState)
+  }, [])
+
   return (
-    <>
-      
-    </>
+    <div className={isDark ? 'dark-mode' : 'light-mode'}>
+      <Header isDark={isDark} onToggle={toggle} />
+    </div>
   )
 }
+
+function Header({ isDark, onToggle }) {
+  return (
+    <div className='header'>
+      <h1>Forecast App</h1>
+      <button onClick={onToggle}>
+        {isDark ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+      </button>
+    </div>
+  )
+}
+
+
+
+
+
 
 export default App
