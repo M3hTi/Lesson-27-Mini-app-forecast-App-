a6d6bff0c464a2aca16bebd41b818a8bbe268406
M3hTi <mehdinasir502@yahoo.com>
2025-02-05 15:09:31 +0330

    feat: implement weather data fetching with WeatherAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
+import axios from 'axios'
 
 import './App.css'
+
+const apiKey = '3eb8b7903c2740898db112352250502'
 
 function App() {
   const [isDark, setIsDark] = React.useState(false)
 
   const [searchTerm, setSearchTerm] = React.useState('')
+  const [confirmSearch, setConfirmSearch] = React.useState('')
 
   const toggle = React.useCallback(() => {
     setIsDark(prevState => !prevState)
@@ -16,15 +20,35 @@ function App() {
     setSearchTerm(newValue)
   },[])
 
+  const searchHandler =  React.useCallback((e) => {
+    e.preventDefault()
+    setConfirmSearch(searchTerm)
+  },[searchTerm])
+
+
+  const fetchData  = React.useCallback(() => {
+    if(!confirmSearch) return
+
+    axios
+      .get(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${confirmSearch}`)
+      .then(response => {
+        if(response.status !== 200) throw new Error("status is not ok!");
+        console.log(response.data);
+      })
+      .catch(error => console.error("Error fetching forecast data:", error.message))
+  },[confirmSearch])
+
+  React.useEffect(() => fetchData(),[fetchData])
+
   return (
     <div className={isDark ? 'dark-mode' : 'light-mode'}>
       <Header isDark={isDark} onToggle={toggle} />
-      <SearchForm value={searchTerm} onInputHandler={inputHandler}/>
+      <SearchForm value={searchTerm} onInputHandler={inputHandler} onSearch={searchHandler}/>
     </div>
   )
 }
 
-function Header({ isDark, onToggle}) {
+function Header({isDark, onToggle}) {
   return (
     <div className='header'>
       <h1>Forecast App</h1>
@@ -40,11 +64,11 @@ function Header({ isDark, onToggle}) {
 
 
 
-function SearchForm({value, onInputHandler}) {
+function SearchForm({value, onInputHandler, onSearch}) {
   return(
-    <form className='padding-32'  >
+    <form className='padding-32' onSubmit={onSearch} >
       <input type="text" className='padding-6' placeholder='Please enter the city name' value={value} onChange={onInputHandler}/>
-      <button type='submit' className='padding-6'>Search</button>
+      <button type='submit' className='padding-8 submit'>Search</button>
     </form>
   )
 }
